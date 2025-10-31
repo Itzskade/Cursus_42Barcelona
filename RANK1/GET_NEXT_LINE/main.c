a9a091b68de47ef07250e625f350a0596ae5498f
Skade <itzskade@gmail.com>
2025-10-31 18:48:50 +0000

    Upload GNL

---
+++ RANK1/GET_NEXT_LINE/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarin-n <rmarin-n@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/31 16:39:17 by rmarin-n          #+#    #+#             */
+/*   Updated: 2025/10/31 18:47:35 by rmarin-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	int		fd;
+
+	fd = open("test.txt", O_RDONLY);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	return (0);
+}
---
+++ RANK1/GET_NEXT_LINE/main_bonus.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarin-n <rmarin-n@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/31 16:39:17 by rmarin-n          #+#    #+#             */
+/*   Updated: 2025/10/31 18:47:13 by rmarin-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	int		fd;
+	int		fd2;
+
+	fd = open("test.txt", O_RDONLY);
+	fd2 = open("test2.txt", O_RDONLY);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd2));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd2));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd2));
+	return (0);
+}
