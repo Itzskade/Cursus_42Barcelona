6873fdb62bf8647729da8ff28bb8f6b8642dabb3
Skade <itzskade@gmail.com>
2025-11-07 12:49:03 +0100

    Update main to read lines from file and handle memory
    Refactor main function to read lines from a file until EOF and free memory.

--- RANK1/GET_NEXT_LINE/main.c
+++ RANK1/GET_NEXT_LINE/main.c
@@ -13,13 +13,21 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-int	main(void)
+int     main(void)
 {
-	int		fd;
+        int     fd;
+        char    *line;
 
-	fd = open("test.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	return (0);
+        fd = open("test.txt", O_RDONLY);
+        if (fd <= 0)
+                return (1);
+        line = get_next_line(fd);
+        while (line)
+        {
+                printf("%s", line);
+                free(line);
+                line = get_next_line(fd);
+        }
+        close(fd);
+        return (0);
 }
